e0c5ba3c47b476453920979e9ce227748f75829a
Dane Schmidt <daneschmidt@gmail.com>
2020-01-30 10:24:38 -0600

    still working on client side GET call

--- build/resources/main/static/scripts/app.js
+++ build/resources/main/static/scripts/app.js
@@ -1,5 +1,60 @@
-$(document).ready(function() {
-    alert('WTF TWO JQUERY STYLE');
-});
+//$(document).ready(function() {
+//    alert('WTF TWO JQUERY STYLE');
+//});
+//
+//alert('WTF');
 
-alert('WTF');
+console.log('client.js sourced');
+
+$( document ).ready( onReady );
+
+function onReady() {
+    console.log('DOM ready');
+//    $('#submitEmployeeButton').on('click', onClickSubmit);
+    getEmployees();
+    console.log(employeeList);
+}
+
+
+// API CALLS
+function getEmployees() {
+    $.ajax({
+        method: 'GET',
+        url: '/api/employees/all'
+
+    })
+    .then(function(response) {
+        render(response);
+
+    })
+    .catch(function(err) {
+        console.log(err);
+
+    })
+}
+
+function postJokeBook(jokes) {
+    $.ajax({
+        method: 'POST',
+        url: '/api/employee/add',
+        data:employeeList
+    })
+    .then(function(response){
+        alert('New employee added!!');
+        getJokeBook();
+    })
+    .catch(function(err){
+        alert("Its busted");
+
+    })
+
+}
+
+//VIEW UPDATE
+function render(employeeList) {
+    const $employeeOutput = $('#outputDiv');
+    employeeOutput.empty()
+    for (let i = 0 ; i < employeeList.length; i ++) {
+        $employeeOutput.append(`<p>${jokes[i].whoseJoke}</p>`);
+}
+}
